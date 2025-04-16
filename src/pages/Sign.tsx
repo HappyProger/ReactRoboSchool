a5aa6d8c7dbfb5cfe7f788ad9e269a8ffbe5175c
nursat <nursatalzanov@gmail.com>
2025-04-16 10:43:25 +0500

    ShadCn integration

--- src/pages/Sign.tsx
+++ src/pages/Sign.tsx
@@ -1,6 +1,22 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Alert, AlertDescription, AlertTitle } from "../components/ui/alert";
+import { CheckCircle } from "lucide-react";
+import { Button } from "../components/ui/button"
 const Sign: React.FC = () => {
+    const [showAlert, setShowAlert] = useState(false);
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [email, setEmail] = useState("");
+
+    const isFormValid = name.trim() !== "" && phone.trim() !== "" && email.trim() !== "";
+
+    const handleClick = () => {
+        if (!isFormValid) return;
+
+        setShowAlert(true);
+        setTimeout(() => setShowAlert(false), 5000);
+    };
+
     return (
         <div id='sign' className="bg-[#D52027] text-white font-medium py-24 px-4">
             <div className="max-w-6xl mx-auto grid sm:grid-cols-1  md:grid-cols-2 items-center">
@@ -19,24 +35,40 @@ const Sign: React.FC = () => {
                     <input
                         type="text"
                         placeholder="Имя"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
                         className="bg-white w-full px-4 py-3 text-black border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
                     <input
                         type="tel"
                         placeholder="Телефон"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
                         className="bg-white w-full px-4 py-3 text-black border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
                     <input
                         type="email"
                         placeholder="E-mail"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                         className="bg-white w-full px-4 py-3 text-black border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
-                    <button
-                        type="submit"
-                        className="w-full px-4 py-3 rounded-sm bg-[#141024] hover:bg-[#2c2c38] transition"
-                    >
-                        Оставить заявку
-                    </button>
+
+                    <Button variant="outline"
+                            type="button"
+                            onClick={handleClick}
+                            disabled={!isFormValid}
+                            className="w-full px-4 py-5 rounded-sm bg-[#141024] hover:bg-[#2c2c38] transition disabled:opacity-50 disabled:cursor-not-allowed">Оставить заявку</Button>
+
+                    {showAlert && (
+                        <Alert className="bg-green-100 border border-green-400 text-green-800">
+                            <CheckCircle className="h-5 w-5" />
+                            <AlertTitle>Заявка отправлена!</AlertTitle>
+                            <AlertDescription>
+                                Мы свяжемся с вами в ближайшее время.
+                            </AlertDescription>
+                        </Alert>
+                    )}
                 </form>
             </div>
         </div>
